9e7340badd806c36f54a085c0938ee5697acfa3e
Bede Constantinides <bedeabc@gmail.com>
2025-10-15 21:57:02 +0100

    Remove reundant revcomp

--- src/minimizers.rs
+++ src/minimizers.rs
@@ -1,4 +1,4 @@
-use packed_seq::{SeqVec, complement_base, unpack_base};
+use packed_seq::{SeqVec, unpack_base};
 
 use crate::filter::Buffers;
 
@@ -10,9 +10,8 @@ pub fn decode_u64(minimizer: u64, k: u8) -> Vec<u8> {
     (0..k)
         .map(|i| {
             let base_bits = ((minimizer >> (2 * i)) & 0b11) as u8;
-            unpack_base(complement_base(base_bits))
+            unpack_base(base_bits)
         })
-        .rev()
         .collect()
 }
 
@@ -21,9 +20,8 @@ pub fn decode_u128(minimizer: u128, k: u8) -> Vec<u8> {
     (0..k)
         .map(|i| {
             let base_bits = ((minimizer >> (2 * i)) & 0b11) as u8;
-            unpack_base(complement_base(base_bits))
+            unpack_base(base_bits)
         })
-        .rev()
         .collect()
 }
 
