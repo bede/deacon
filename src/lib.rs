50c044d64f6f191037008f167d9d120b06fad808
JeremyWesthead <jeremywesthead3@gmail.com>
2026-03-09 13:10:51 +0000

    fix(python): add __version__

--- src/lib.rs
+++ src/lib.rs
@@ -45,6 +45,7 @@ fn deacon(m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(index_info, m)?)?;
     m.add_function(wrap_pyfunction!(index_intersect, m)?)?;
     m.add_function(wrap_pyfunction!(index_union, m)?)?;
+    m.add("__version__", env!("CARGO_PKG_VERSION"))?;
 
     #[cfg(feature = "fetch")]
     m.add_function(wrap_pyfunction!(index_fetch, m)?)?;
